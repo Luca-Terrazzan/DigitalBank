7f5a556a3f0967eb3bcb4d8bc16ea4af29125ea5
Rezzaghi <mrezzaghi@deloitte.it>
2017-03-13 11:17:27 +0100

    Retail related list v0.1

---
+++ src/aura/RetailList/RetailList.app
@@ -0,0 +1,8 @@
+<aura:application >
+	
+	<ltng:require styles="{!$Resource.slds + '/assets/styles/salesforce-lightning-design-system.min.css'}" />
+	
+	<!-- <c:Expenses /> -->
+	<c:Retail_Related_List recordId="00146000002oWD8AAM"/>
+
+</aura:application>
--- src/aura/Retail_Related_List/Retail_Related_ListController.js
+++ src/aura/Retail_Related_List/Retail_Related_ListController.js
@@ -1,5 +1,17 @@
 ({
-	myAction : function(component, event, helper) {
-		
+	doInit : function(component) {
+		var action = component.get("{!c.getRetailFinancialAccount}");
+		// action.setParams({ account: component.get("v.account") }); // to be enabled once the component is finished
+		action.setParams({ account: "00146000002oWD8AAM"}); // temp hardocding
+
+		action.setCallback(this, function(response){
+			var state = response.getState();
+			var result = response.getReturnValue();
+			console.log("APEX controller SOQL result: " , result);
+			if( component.isValid() && state === "SUCCESS" ){
+				component.set("v.financialAccounts", result);
+			} else console.log("fail: " + state);
+		});
+		$A.enqueueAction(action);
 	}
 })
