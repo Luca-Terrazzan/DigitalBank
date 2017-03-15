afccdf33d0258b79cadf4c1262489e3b3c65b28c
Rezzaghi <mrezzaghi@deloitte.it>
2017-03-15 15:53:36 +0100

    aggiunto opportunity related list controller

---
+++ src/aura/Opportunity_Related_List/Opportunity_Related_ListController.js
@@ -0,0 +1,17 @@
+({
+    doInit : function(component) {
+        var action = component.get("{!c.getRetailOpportunities}");
+        action.setParams({ accountId: component.get("v.recordId") }); // to be enabled once the component is finished
+        //action.setParams({ accountId: "00146000002oWD8AAM"}); // temp hardocding
+
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            var result = response.getReturnValue();
+            console.log("APEX controller SOQL result: " , result);
+            if( component.isValid() && state === "SUCCESS" ){
+                component.set("v.financialAccounts", result);
+            } else console.log("fail: " + state);
+        });
+        $A.enqueueAction(action);
+    }
+})
