b0c2284ef52c17ffa3725a934ca7030ab5a189c5
Luca Terrazzan <luca.terrazzan@telnext.com>
2017-03-16 13:18:37 +0100

    Refactor
    Refactor e miglioramento logica related list financial accounts

--- src/aura/Retail_Related_List/Retail_Related_ListController.js
+++ src/aura/Retail_Related_List/Retail_Related_ListController.js
@@ -1,17 +1,22 @@
 ({
-	doInit : function(component) {
-		var action = component.get("{!c.getRetailFinancialAccount}");
-		action.setParams({ accountId: component.get("v.recordId") }); // to be enabled once the component is finished
-		//action.setParams({ accountId: "00146000002oWD8AAM"}); // temp hardocding
+    doInit : function(component, event, helper) {
+        var faCategory = component.get("v.category");
+        var action = component.get("{!c.getFinancialAccount}");
+        action.setParams({
+            accountId: component.get("v.recordId"),
+            category: helper.validateCategory(faCategory)
+        });
 
-		action.setCallback(this, function(response){
-			var state = response.getState();
-			var result = response.getReturnValue();
-			console.log("APEX controller SOQL result: " , result);
-			if( component.isValid() && state === "SUCCESS" ){
-				component.set("v.financialAccounts", result);
-			} else console.log("fail: " + state);
-		});
-		$A.enqueueAction(action);
-	}
+        console.log("calling selector with data: " + helper.validateCategory(faCategory));
+
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            var result = response.getReturnValue();
+            console.log("APEX controller SOQL result: " , result);
+            if( component.isValid() && state === "SUCCESS" ){
+                component.set("v.financialAccounts", result);
+            } else console.log("fail: " + state);
+        });
+        $A.enqueueAction(action);
+    }
 })
